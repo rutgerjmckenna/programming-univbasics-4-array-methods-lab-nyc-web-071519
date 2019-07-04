3d7573a8640b42f8c73ac7b5346579e17fcfdeac
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-04 01:31:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,11 +13,12 @@ def using_reverse(array)
 end
 
 def using_first(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.first
 end
 
 def using_last(array)
-
+  
 end
 
 def using_size(array)
